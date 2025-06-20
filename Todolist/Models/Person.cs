4cb8468359e0e12b9193e14da13e5d7f8854377e
amaresh888 <kamalapuramsureshbabu55637@gmail.com>
2025-06-20 15:31:41 +0530

    added the data anotaions

--- Todolist/Models/Person.cs
+++ Todolist/Models/Person.cs
@@ -1,11 +1,29 @@
-﻿namespace Todolist.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Todolist.Models
 {
     public class Person
     {
+        [Key] // This indicates 'id' is the primary key
         public int id { get; set; }
+
+        [Required(ErrorMessage = "Name is required.")]
+        [StringLength(100, ErrorMessage = "Name can't exceed 100 characters.")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Task description is required.")]
+        [StringLength(200, ErrorMessage = "Task can't exceed 200 characters.")]
         public string Task { get; set; }
+
+        [Required(ErrorMessage = "Start Date is required.")]
+        [DataType(DataType.Date)]
+        [Display(Name = "Start Date")]
         public DateTime StartDate { get; set; }
+
+        [Required(ErrorMessage = "End Date is required.")]
+        [DataType(DataType.Date)]
+        [Display(Name = "End Date")]
         public DateTime EndDate { get; set; }
     }
 }
